006dc63a25f1c8dc5eaf09773d622183c5a7da7f
Sergey Vinogradov <grapesh@gmail.com>
2018-04-25 11:52:23 -0400

    Update atcf.py

--- csdlpy/atcf/atcf.py
+++ csdlpy/atcf/atcf.py
@@ -7,7 +7,7 @@ Created on Thu Mar 16 13:24:14 2017
 from datetime import datetime
 
 #==============================================================================
-def readTrack ( atcfFile ):
+def readTrack ( atcfFile, product=None ):
     """
     Reads ATCF-formatted file
     Args:
@@ -15,7 +15,9 @@ def readTrack ( atcfFile ):
     Returns:
         dict: 'lat', 'lon', 'vmax', 'mslp','dates'
     """
-        
+    
+    print '[info]: reading ATCF file ', atcfFile
+    
     myOcn  = []
     myCy   = []
     myDate = []
@@ -23,7 +25,7 @@ def readTrack ( atcfFile ):
     myLon  = []
     myVmax = []
     myMSLP = []
-    
+    myProduct = [] 
     fileOK = True
     try:
         lines = open(atcfFile).readlines()
@@ -34,23 +36,36 @@ def readTrack ( atcfFile ):
     if fileOK:
         for line in lines:
             r = line.rstrip().split(',')
-            myOcn.append  (r[0])
-            myCy.append   (int(r[1]))
-            myDate.append (datetime.strptime(r[2].strip(),'%Y%m%d%H'))
-            latSign = -1.0
-            if 'N' in r[6]:
-                latSign = 1.0     
-            myLat.append  (latSign*0.1*float(r[6][:-1]))
-            lonSign = -1.0
-            if 'E' in r[7]:
-                lonSign = 1.0
-            myLon.append  (lonSign*0.1*float(r[7][:-1]))
-            myVmax.append (float(r[8]))
-            myMSLP.append (float(r[9]))
+
+            prod = r[4].strip()
+
+            if product is None or prod in product:
+                
+                myOcn.append  (r[0])
+                myCy.append   (int(r[1]))
+                myDate.append (datetime.strptime(r[2].strip(),'%Y%m%d%H'))
+
+                latSign = -1.0
+                if 'N' in r[6]:
+                    latSign = 1.0     
+                myLat.append  (latSign*0.1*float(r[6][:-1]))
+
+                lonSign = -1.0
+                loopOver   = 0.
+                if 'E' in r[7]:
+                    lonSign = 1.0
+                    loopOver = -360.
+                myLon.append  (lonSign*0.1*float(r[7][:-1])+loopOver)
+
+                myVmax.append (float(r[8]))
+                myMSLP.append (float(r[9]))
+                myProduct.append(prod)
     
     return { 
             'basin' : myOcn,    'cy' : myCy, 'dates' : myDate, 
             'lat'   : myLat,   'lon' : myLon,
-            'vmax'  : myVmax, 'mslp' : myMSLP }
+            'vmax'  : myVmax, 'mslp' : myMSLP,
+            'product' : myProduct }
+
 
     
