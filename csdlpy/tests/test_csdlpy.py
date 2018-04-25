7d9a9b28b6236ebdf58e9b61a643215877d0f3ff
Sergey Vinogradov <grapesh@gmail.com>
2018-04-25 13:26:12 -0400

    Update test_csdlpy.py

--- csdlpy/tests/test_csdlpy.py
+++ csdlpy/tests/test_csdlpy.py
@@ -7,10 +7,10 @@ Created on Thu Mar 16 10:02:14 2017
 import sys
 
 #==============================================================================
-def run_test_csdlpy (argv):
+def test_csdlpy (argv):
     print '[warn]: Test suite not yet implemented'
 
 #==============================================================================
 if __name__ == "__main__":
-    run_test_csdlpy (sys.argv[1:])    
+    test_csdlpy (sys.argv[1:])    
     
