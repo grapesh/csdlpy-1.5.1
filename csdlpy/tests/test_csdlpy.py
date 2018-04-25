7ded55f41d1c7eefee909bb60d969a1890a3ee87
Sergey Vinogradov <grapesh@gmail.com>
2018-04-25 13:25:55 -0400

    Update test_csdlpy.py

--- csdlpy/tests/test_csdlpy.py
+++ csdlpy/tests/test_csdlpy.py
@@ -4,18 +4,12 @@ Created on Thu Mar 16 10:02:14 2017
 
 @author: Sergey.Vinogradov
 """
-
-import os
-import getopt
-import matplotlib
-import numpy as np
-matplotlib.use('Agg',warn=False)
-import matplotlib.pyplot as plt
 import sys
-from datetime import datetime as dt
+
 #==============================================================================
 def run_test_csdlpy (argv):
-     print '[warn]: Test suite not yet implemented'
+    print '[warn]: Test suite not yet implemented'
+
 #==============================================================================
 if __name__ == "__main__":
     run_test_csdlpy (sys.argv[1:])    
