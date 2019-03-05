39ef40791a2ae986d8aea9778508f376e7a6c94b
Sergey Vinogradov <grapesh@gmail.com>
2019-03-05 17:02:35 -0500

    Added csdlpy/etss

--- csdlpy/__init__.py
+++ csdlpy/__init__.py
@@ -6,7 +6,8 @@ import plotter
 import interp
 import valstat
 import transfer
+import etss
 from .version import __version__
-__all__ = ['adcirc', 'estofs', 'obs', 'atcf', 'plotter', 'interp', 'valstat','transfer']
+__all__ = ['adcirc', 'estofs', 'obs', 'atcf', 'plotter', 'interp', 'valstat', 'transfer','etss']
 
 
